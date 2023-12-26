eabffe5c66d77dc2d6d1d6ab158540f8be339106
khawlakha12 <kharbouchikhawla603@gmail.com>
2023-12-26 11:42:58 +0100

    front-end

--- views/page/home.php
+++ views/page/home.php
@@ -8,86 +8,6 @@
     <link rel="stylesheet" href="../style/home.css">
     <link rel="icon" href="logo.png" type="image/png">
     <title>home</title>
-    <style>
-  button {
-    &.dark {
-        --background: #2F3545;
-        --shadow: 0 2px 8px -1px #{rgba(#151924, .32)};
-        --shadow-hover: 0 4px 20px -2px #{rgba(#151924, .5)};
-    }
-    &.white {
-        --background: #fff;
-        --text: #275efe;
-        --shadow: 0 2px 8px -1px #{rgba(#121621, .04)};
-        --shadow-hover: 0 4px 20px -2px #{rgba(#121621, .12)};
-    }
-    &.fast {
-        --duration: .32s;
-    }
-}
-
-button {
-    --background: #275efe;
-    --text: #fff;
-    --font-size: 16px;
-    --duration: .44s;
-    --move-hover: -4px;
-    --shadow: 0 2px 8px -1px #{rgba(#275efe, .32)};
-    --shadow-hover: 0 4px 20px -2px #{rgba(#275efe, .5)};
-    --font-shadow: var(--font-size);
-    padding: 16px 32px;
-    font-family: 'Roboto';
-    font-weight: 500;
-    line-height: var(--font-size);
-    border-radius: 24px;
-    display: block;
-    outline: none;
-    appearance: none;
-    border: none;
-    text-decoration: none;
-    font-size: var(--font-size);
-    letter-spacing: .5px;
-    background: var(--background);
-    color: var(--text);
-    box-shadow: var(--shadow);
-    transform: translateY(var(--y)) translateZ(0);
-    transition: transform var(--duration) ease, box-shadow var(--duration) ease;
-    div {
-        display: flex;
-        overflow: hidden;
-        text-shadow: 0 var(--font-shadow) 0 var(--text);
-        span {
-            display: block;
-            backface-visibility: hidden;
-            font-style: normal;
-            transition: transform var(--duration) ease;
-            transform: translateY(var(--m)) translateZ(0);
-            $i: 1;
-            @while $i < 12 {
-                &:nth-child(#{$i}) {
-                    transition-delay: $i / 20 + s;
-                }
-                $i: $i + 1;
-            }
-        }
-    }
-    &:hover {
-        --y: var(--move-hover);
-        --shadow: var(--shadow-hover);
-        span {
-            --m: calc(var(--font-size) * -1);
-        }
-    }
-    &.reverse {
-        --font-shadow: calc(var(--font-size) * -1);
-        &:hover {
-            span {
-                --m: calc(var(--font-size));
-            }
-        }
-    }
-}
-    </style>
 </head>
 
 <body>
@@ -168,15 +88,7 @@ button {
           <span>9h ago</span>
         </div>
       </div>
-      <div class="message-wrapper reverse">
-        <img class="message-pp" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile-pic">
-        <div class="message-box-wrapper">
-          <div class="message-box">
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </div>
-          <span>9h ago</span>
-        </div>
-      </div>
+
       <div class="message-wrapper">
         <img class="message-pp" src="https://images.unsplash.com/photo-1587080266227-677cc2a4e76e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=934&amp;q=80" alt="profile-pic">
         <div class="message-box-wrapper">
@@ -260,8 +172,7 @@ button {
       </button>
       
         <input type="text" class="chat-input" placeholder="Enter your message here">
-<div class="button-list">
-<button class="fast white">send</button>
+<button type="submit" class="message-submit">Send</button>
 </div>
      </div>
   </div>
@@ -460,9 +371,7 @@ themeColors.forEach(themeColor => {
   });
 });
 </script>
-<script>
-  document.querySelectorAll('button').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
-</script>
+
 </body>
 
 </html>
